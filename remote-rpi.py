4eb0d64fa422786d1c19d4c0d64196cb1ea24519
philc <philc@arcx.com>
2019-06-03 15:13:45 -0400

    first commit

--- remote-rpi.py
+++ remote-rpi.py
@@ -6,13 +6,15 @@ GPIO.setup(21, GPIO.OUT)
 pin = 21
 
 app = Flask(__name__)
-	
+
+
 @app.route('/GPIO/<state>', methods={'GET', 'POST'})
 def red_light(state):
-	if state == 'on':
-		GPIO.output(pin, GPIO.HIGH)
-	elif state == 'off':
-		GPIO.output(pin, GPIO.LOW)
-	
+    if state == 'on':
+        GPIO.output(pin, GPIO.HIGH)
+    elif state == 'off':
+        GPIO.output(pin, GPIO.LOW)
+
+
 if __name__ == '__main__':
-	app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0')
