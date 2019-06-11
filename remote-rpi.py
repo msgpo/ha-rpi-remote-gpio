1cbef79e5e00424311856908a498d162a547960d
philc <philc@arcx.com>
2019-06-11 10:38:37 -0400

    first commit

--- remote-rpi.py
+++ remote-rpi.py
@@ -1,19 +1,22 @@
 from flask import Flask
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(21, GPIO.OUT)
-pin = 21
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(21, GPIO.OUT)
+# pin = 21
 
 app = Flask(__name__)
 
 
-@app.route('/GPIO/<state>', methods={'GET', 'POST'})
-def red_light(state):
-    if state == 'on':
-        GPIO.output(pin, GPIO.HIGH)
-    elif state == 'off':
-        GPIO.output(pin, GPIO.LOW)
+@app.route('/GPIO/<strCommand>', methods={'GET', 'POST'})
+def relay_control(strCommand):
+    print('Received: ' + strCommand)
+    if strCommand == 'on':
+        print('On Processed')
+        # GPIO.output(pin, GPIO.HIGH)
+    elif strCommand == 'off':
+        print('Off Processed')
+        # GPIO.output(pin, GPIO.LOW)
 
 
 if __name__ == '__main__':
